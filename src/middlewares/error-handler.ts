a2c19d5fa622658e27e191a685ec6028dd223f15
AkkalDhami <dhamiakkal21@gmail.com>
2025-11-13 08:32:16 +0545

    add global error handler middleware

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { env } from "#configs/env.js";
+import { logger } from "#utils/logger.js";
+import { ApiError } from "../utils/api-error";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = "Internal server error";
+
+  if (err instanceof ApiError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  }
+
+  logger.error(
+    `Error: ${message} | Status: ${statusCode} | Path: ${req.method} ${req.originalUrl}`,
+    err
+  );
+
+  const response = {
+    success: false,
+    message,
+    ...(env.NODE_ENV === "development" && { stack: err.stack }),
+  };
+
+  res.status(statusCode).json(response);
+};
