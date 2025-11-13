4f61d36a47f8c04a8c20afc157915c1e8bc05a18
AkkalDhami <dhamiakkal21@gmail.com>
2025-11-13 08:31:36 +0545

    add auth routes

---
+++ src/routes/v1/auth.routes.ts
@@ -0,0 +1,71 @@
+import {
+  changePassword,
+  deleteAccount,
+  getUserProfile,
+  googleSignin,
+  logout,
+  reactivateAccount,
+  refreshToken,
+  resetPassword,
+  signinUser,
+  signupUser,
+  updateProfile,
+} from "#controllers/auth.controller.js";
+import { sendOtp, verifyOtp } from "#controllers/otp.controller.js";
+import {
+  changePasswordLimiter,
+  deleteAccountLimiter,
+  otpRequestLimiter,
+  otpVerificationLimiter,
+  resetPasswordLimiter,
+  signinLimiter,
+  signupLimiter,
+} from "#lib/rate-limiter.js";
+import upload from "#middlewares/upload-file.js";
+import { isAuthenticated } from "#middlewares/verify-authentication.js";
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/profile", isAuthenticated, getUserProfile);
+
+router.post("/signup", signupLimiter, signupUser);
+router.post("/signin", signinLimiter, signinUser);
+
+router.post("/request-otp", otpRequestLimiter, sendOtp);
+router.post("/verify-otp", otpVerificationLimiter, verifyOtp);
+
+router.post(
+  "/reset-password",
+  isAuthenticated,
+  resetPasswordLimiter,
+  resetPassword
+);
+router.post(
+  "/change-password",
+  isAuthenticated,
+  changePasswordLimiter,
+  changePassword
+);
+
+router.patch(
+  "/update-profile",
+  upload.single("avatar"),
+  isAuthenticated,
+  updateProfile
+);
+
+router.post("/google-signin", googleSignin);
+
+router.post("/refresh-token", refreshToken);
+router.post("/logout", isAuthenticated, logout);
+
+router.delete(
+  "/delete-account",
+  isAuthenticated,
+  deleteAccountLimiter,
+  deleteAccount
+);
+router.put("/reactivate-account", isAuthenticated, reactivateAccount);
+
+export default router;
